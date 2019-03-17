983e6c5f04be85f8f131d464d02b909bc5bd6a6d
benhaz <benhaz1024@gmail.com>
2019-03-17 19:25:03 +0800

    download package to dl directory & not delate package.

--- prepare-env.sh
+++ prepare-env.sh
@@ -1,55 +1,49 @@
 #!/bin/sh
-[ ! -d "./arm-trusted-firmware" ] && {
+[ ! -f "./dl/arm-trusted-firmware.tar.gz" ] && {
 	echo  "\033[32mDownload arm-trusted-firmware\n\033[0m"
-	wget -O arm-trusted-firmware.tar.gz https://github.com/ARM-software/arm-trusted-firmware/archive/v2.0.tar.gz
-	tar zxf arm-trusted-firmware.tar.gz && {
+	wget -O dl/arm-trusted-firmware.tar.gz https://github.com/ARM-software/arm-trusted-firmware/archive/v2.0.tar.gz	
+	tar zxf dl/arm-trusted-firmware.tar.gz && {
 		mv arm-trusted-firmware-* arm-trusted-firmware
-		rm arm-trusted-firmware.tar.gz
 	}
 }
 
-[ ! -d "./u-boot" ] && {
+[ ! -f "./dl/u-boot.tar.gz" ] && {
 	echo  "\033[32mDownload u-boot \n\033[0m"
-	wget -O u-boot.tar.gz https://github.com/u-boot/u-boot/archive/v2018.11.tar.gz
-	tar zxf u-boot.tar.gz && {
+	wget -O dl/u-boot.tar.gz https://github.com/u-boot/u-boot/archive/v2018.11.tar.gz
+	tar zxf dl/u-boot.tar.gz && {
 		mv u-boot-* u-boot
-		rm u-boot.tar.gz
 	}
 }
 
-[ ! -d "./optee_os" ] && {
+[ ! -f "./dl/optee_os.tar.gz" ] && {
 	echo  "\033[32mDownload optee_os \n\033[0m"
-	wget -O optee_os.tar.gz https://github.com/OP-TEE/optee_os/archive/3.4.0.tar.gz
-	tar zxf optee_os.tar.gz && {
+	wget -O dl/optee_os.tar.gz https://github.com/OP-TEE/optee_os/archive/3.4.0.tar.gz
+	tar zxf dl/optee_os.tar.gz && {
 		mv optee_os-* optee_os
-		rm optee_os.tar.gz
 	}
 	sed -i /ta_arm32/d optee_os/core/arch/arm/plat-rpi3/conf.mk
 }
 
-[ ! -d "./optee_client" ] && {
+[ ! -f "./dl/optee_client.tar.gz" ] && {
 	echo  "\033[32mDownload optee_client \n\033[0m"
-	wget -O optee_client.tar.gz https://github.com/OP-TEE/optee_client/archive/3.3.0.tar.gz
-	tar zxf optee_client.tar.gz && {
+	wget -O dl/optee_client.tar.gz https://github.com/OP-TEE/optee_client/archive/3.3.0.tar.gz
+	tar zxf dl/optee_client.tar.gz && {
 		mv optee_client-* optee_client
-		rm optee_client.tar.gz
 	}
 }
 
-[ ! -d "./optee_examples" ] && {
+[ ! -f "./dl/optee_examples.tar.gz" ] && {
 	echo  "\033[32mDownload optee_examples \n\033[0m"
-	wget -O optee_examples.tar.gz https://github.com/linaro-swg/optee_examples/archive/3.3.0.tar.gz
-	tar zxf optee_examples.tar.gz && {
+	wget -O dl/optee_examples.tar.gz https://github.com/linaro-swg/optee_examples/archive/3.3.0.tar.gz
+	tar zxf dl/optee_examples.tar.gz && {
 		mv optee_examples-* optee_examples
-		rm optee_examples.tar.gz
 	}
 }
-[ ! -d "./linux" ] && {
+[ ! -f "./dl/linux.tar.gz" ] && {
 	echo  "\033[32mDownload linux\n\033[0m"
-	wget -O linux.tar.gz https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20190215-1.tar.gz
-	tar zxf linux.tar.gz && {
+	wget -O dl/linux.tar.gz https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20190215-1.tar.gz
+	tar zxf dl/linux.tar.gz && {
 		mv linux-* linux
-		rm linux.tar.gz
 	}
 }
 
